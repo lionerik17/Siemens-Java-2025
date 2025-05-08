594e239ed25b6042315c5b0e9264b46aeeca7adf
lionerik17 <pufferstar17@gmail.com>
2025-05-08 15:22:13 +0300

    added non-empty on Item class fields

--- src/main/java/com/siemens/internship/model/Item.java
+++ src/main/java/com/siemens/internship/model/Item.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,8 +22,11 @@ public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotEmpty
     private String name;
+    @NotEmpty
     private String description;
+    @NotEmpty
     private String status;
 
     // validating email using custom annotation
